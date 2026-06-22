f4ce1aa69195db6012e61d946d63f0cc2eb042f0
Graham Warner <10564844+gcwarner1@users.noreply.github.com>
2026-06-22 11:58:04 -0700

    Update text_me.py
    Update header information

--- text_me.py
+++ text_me.py
@@ -6,13 +6,12 @@ Usage:
 	textme = textMe(number=yournumber, carrier=yourcarrier)
 	textme.run()
 
-If you do not have a SMTP server on your network (if you are at the Martinos Center you do) you can use your gmail account
-by using:
+If you do not have a SMTP server on your network you can use your gmail account by using:
 	textme = textMe(number=yournumber, carrier=yourcarrier, gmail=you@gmail.com, password=yourgmailpassword)
 	textme.run()
 
 author: Graham Warner
-email: gwarner@mgh.harvard.edu
+email: grahamwarner@protonmail.com
 '''
 
 import atexit, os, smtplib, re, sys, __main__
